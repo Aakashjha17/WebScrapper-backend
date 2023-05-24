996891c6ed3657bda104351bd88517eeb0b6b457
Aakashjha17 <80976345+Aakashjha17@users.noreply.github.com>
2023-05-24 09:35:14 +0530

    Changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import WebScrapperRoutes from './routes/webscrapperRoutes.js'
 dotenv.config()
 const app=express()
 
-const port=process.env.PORT
+const port=process.env.PORT || 8000
 const DATABASE_URL=process.env.DATABASE_URL
 
 app.use(cors())
